802d0774cf0035622d69a7633ab96533b79c9425
Jonas Scholz <jonas.scholz123@googlemail.com>
2021-03-22 12:01:47 +0000

    cleanup + get rid of magic numbers

--- src/model.py
+++ src/model.py
@@ -1,30 +1,50 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from utils import conv_output_shape
 
 class Model(nn.Module):
 
     def __init__(self):
         # TODO: pass hyperparameters
 
+        #cnn params
+        kernel_size = 5
+        pool_size = 2
+
+        #cnn channels
+        in_channels = 3
+        out_channels_1 = 6
+        out_channels_2 = 16
+
+        #linear params
+        in_lin1 = 21904 # = 148 ** 2 taken from summary
+        out_lin1 = 120
+        out_lin2 = 84
+        final_out = 2 # boomer or not boomer
+
         super(Model, self).__init__()
 
         # convolutional layers encode pixels into image features 
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.conv2 = nn.Conv2d(6, 16, 5)
+        # 3 in channels, 6 out channels, kernel_size=5
+        self.conv1 = nn.Conv2d(in_channels, out_channels_1, kernel_size)
+        # 6 in channels, 16 out channels, kernel_size=5
+        self.conv2 = nn.Conv2d(out_channels_1, out_channels_2, kernel_size)
 
         # pool
-        self.pool = nn.MaxPool2d(2, 2)
+        self.pool = nn.MaxPool2d(pool_size, pool_size)
 
         # flatten
         self.flat = nn.Flatten()
 
         # fully connected layers make the actual classification
-        self.fc1 = nn.Linear(16 * 5 * 5, 120) #
-        self.fc2 = nn.Linear(120, 84)
+        # 148 taken from summary, as there exists no utility
+        # function for calculating this magic number
+        self.fc1 = nn.Linear(in_lin1, out_lin1)
+        self.fc2 = nn.Linear(out_lin1, out_lin2)
 
-        # single output: boomer percentage 
-        self.fc3 = nn.Linear(84, 10)
+        # output: boomer or not boomer
+        self.fc3 = nn.Linear(out_lin2, final_out)
     
     def forward(self, x):
         '''
@@ -35,7 +55,6 @@ class Model(nn.Module):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
         x = self.flat(x)
-        # x = x.view(-1, 16 * 5 * 5)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
@@ -43,8 +62,18 @@ class Model(nn.Module):
 
 if __name__ == "__main__":
     model = Model()
-    test_images = torch.rand(25, 3, 32, 32) # 25 images with 3 channels of 32*32 pixels
-    output = model(test_images)
+    print(model)
 
-    print(output)
+    from dataloader import BoomerDatasetContainer
+    dataset = BoomerDatasetContainer(is_training=True)
+
+    trainloader = torch.utils.data.DataLoader(dataset,
+                                                batch_size=4,
+                                                shuffle=True,
+                                                num_workers=2)
+
+    iterator = iter(trainloader)
+    output = model(iterator.next()[0])
+    # test_images = torch.rand(25, 3, 32, 32) # 25 images with 3 channels of 32*32 pixels
+    # output = model(test_images)
 
