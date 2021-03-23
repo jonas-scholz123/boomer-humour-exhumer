ff30742a81ebf39dcf556e4f85d03261e2923864
Jonas Scholz <jonas.scholz123@googlemail.com>
2021-03-23 00:25:51 +0000

    remove not applicable classes

--- src/test.py
+++ src/test.py
@@ -1,17 +1,17 @@
 import torch
 import torchvision
 from torchvision import transforms
+from dataloader import BoomerDatasetContainer
 
 from model import Model
 from utils import imshow
 
 class TestSuite:
 
-    def __init__(self, model, testloader, classes):
+    def __init__(self, model, testloader):
         self.model = model
         self.testloader = testloader
         self.batch_size = testloader.batch_size
-        self.classes = classes
     
     def preview_batch(self):
         dataiter = iter(self.testloader)
@@ -19,12 +19,12 @@ class TestSuite:
 
         #print images
         imshow(torchvision.utils.make_grid(x))
-        print('GroundTruth: ', ' '.join('%5s' % self.classes[y[j]] for j in range(self.batch_size)))
+        print('GroundTruth: ', ' '.join('%5s' % y[j] for j in range(self.batch_size)))
 
         y_hat = self.model(x)
 
         _, predicted = torch.max(y_hat, 1)
-        print('Predicted: ', ' '.join('%5s' % self.classes[predicted[j]]
+        print('Predicted: ', ' '.join('%5s' % predicted[j]
                                 for j in range(self.batch_size)))
     
     def calculate_acccuracy(self):
@@ -34,11 +34,13 @@ class TestSuite:
             for data in self.testloader:
                 x, y = data
                 y_hat = self.model(x)
-                _, predicted = torch.max(y_hat.data, 1)
+                # predicted = torch.round(y_hat)
+                _, predicted = torch.max(y_hat, 1)
+                print(y, predicted)
                 total += y.size(0)
                 correct += (predicted == y).sum().item()
 
-        print('Accuracy of the network on the 10000 test images: %d %%' % (
+        print('Accuracy of the network on the ', len(self.testloader), ' test images: %d %%' % (
             100 * correct / total))
 
 class TestSuiteCifar(TestSuite):
@@ -63,10 +65,25 @@ class TestSuiteCifar(TestSuite):
 
         super().__init__(model, testloader, classes)
 
+class TestSuiteBoomer(TestSuite):
+
+    def __init__(self):
+
+        transform = None
+        batch_size = 10
+
+        testset = BoomerDatasetContainer(is_training=False)
+        testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+                                        shuffle=True, num_workers=2)
+
+        model = Model()
+        model.load_state_dict(torch.load("../saved_models/boomer.pth"))
+
+        super().__init__(model, testloader)
 
 if __name__ == "__main__":
-    test_suite = TestSuiteCifar()
-    # test_suite.calculate_acccuracy()
+    test_suite = TestSuiteBoomer()
+    test_suite.calculate_acccuracy()
     test_suite.preview_batch()
 
 
